e5cecbd185677e2347bb40770620b881d1382ce7
Skeo <skeo051600@gmail.com>
2024-04-27 11:20:11 -0500

    Add data to studentData.js and export to main.js

---
+++ studentData.js
@@ -0,0 +1,44 @@
+export const studentData = [
+  {
+    id: 1,
+    image: "testimage1",
+    name: "draven darkwood",
+    house: "slytherin",
+  },
+  {
+    id: 2,
+    image: "testimage2",
+    name: "seraphina nightshade",
+    house: "gryffindor",
+  },
+  {
+    id: 3,
+    image: "testimage3",
+    name: "thalia starling",
+    house: "hufflepuff",
+  },
+  {
+    id: 4,
+    image: "testimage4",
+    name: "aurora frostbloom",
+    house: "ravenclaw",
+  },
+  {
+    id: 5,
+    image: "testimage5",
+    name: "ember duskmourn",
+    house: "slytherin",
+  },
+  {
+    id: 6,
+    image: "testimage6",
+    name: "orion lightfoot",
+    house: "hufflepuff",
+  },
+  {
+    id: 7,
+    image: "testimage7",
+    name: "aria timeweaver",
+    house: "gryffindor",
+  },
+];
