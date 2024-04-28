8fa4c766d9bae1619a6576414bc18713020108db
Skeo <skeo051600@gmail.com>
2024-04-28 15:37:19 -0500

    Create voldemortData and render

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 import { studentData } from "./data/studentData.js";
+import { voldemortData } from "./data/voldemortData.js";
 import { renderToDom } from "./utils/renderToDom.js";
 import { card } from "./utils/cards.js";
 
-const renderStudentCards = (studentData) => {
+const renderCards = (studentData) => {
   let domString = "";
+  let voldemortDomString = "";
 
   studentData.forEach((student) => {
     domString += card(student);
   });
 
+  voldemortData.forEach((expelled) => {
+    voldemortDomString += card(expelled);
+  });
+
   renderToDom("#studentCards", domString);
+  renderToDom("#voldemortCards", voldemortDomString);
 };
 
-renderStudentCards(studentData);
+renderCards(studentData);
 // id and html
