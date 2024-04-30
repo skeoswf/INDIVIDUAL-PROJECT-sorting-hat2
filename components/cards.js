22396c0e8da53c1badc145bfe9ce8589a61669b9
Skeo <skeo051600@gmail.com>
2024-04-30 16:20:22 -0500

    Add design elements Add background Added card styling Add form styling Add button styling Title styling

--- components/cards.js
+++ components/cards.js
@@ -2,12 +2,12 @@ export const card = (student) => {
   return `
   <div class="card" style="width: 16rem;">
     <div class="card-body">
-      <h5 class="card-title">${student.name}</h5>
+      <h5 class="card-title">${student.name.toUpperCase()}</h5>
       <h6 class="card-subtitle mb-2 text-body-secondary">${student.house}</h6>
       <p class="card-text">
         ${student.description}
       </p>
-      <button id="delete--${student.id}">
+      <button class="studentCardButton" id="delete--${student.id}">
         expel
       </button>
     </div>
